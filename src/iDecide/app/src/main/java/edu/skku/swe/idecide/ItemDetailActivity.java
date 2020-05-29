8feb37431f093e9d109971b981055f7e6ced00ba
contestpark <contestpark@naver.com>
2020-05-29 17:20:47 +0900

    add radar graph in ITEM DETAIL page

--- src/iDecide/app/src/main/java/edu/skku/swe/idecide/ItemDetailActivity.java
+++ src/iDecide/app/src/main/java/edu/skku/swe/idecide/ItemDetailActivity.java
@@ -5,32 +5,24 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.LinearLayout;
+import android.view.WindowManager;
 
-import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.components.MarkerView;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.RadarData;
 import com.github.mikephil.charting.data.RadarDataSet;
 import com.github.mikephil.charting.data.RadarEntry;
-import com.github.mikephil.charting.formatter.DefaultAxisValueFormatter;
-import com.github.mikephil.charting.formatter.IValueFormatter;
-import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.IRadarDataSet;
-import com.github.mikephil.charting.utils.ViewPortHandler;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.List;
 
 public class ItemDetailActivity extends AppCompatActivity {
-    private RadarChart hardware_chart;
-    private RadarChart review_chart;
+    private RadarChart chart;
     int hardwareColor = 0xFF64B5F6;
     int reviewColor = 0xFF9575CD;
 
@@ -46,146 +38,123 @@ public class ItemDetailActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
-        setChart();
+        // 전체화면
+        // getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setTitle("RadarChartActivity");
+
+        chart = findViewById(R.id.chart_item_detail);
+        //chart.setBackgroundColor(Color.rgb(60, 65, 82));
+
+        chart.getDescription().setEnabled(false);
+
+        chart.setWebLineWidth(1f);
+        chart.setWebColor(Color.LTGRAY);
+        chart.setWebLineWidthInner(1f);
+        chart.setWebColorInner(Color.LTGRAY);
+        chart.setWebAlpha(80);
+        chart.setTouchEnabled(false);
 
 
-    }
+        setData();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        switch (item.getItemId()) {
-            case android.R.id.home:{
-                finish();
-                return true;
-            }
-        }
-        return super.onOptionsItemSelected(item);
-    }
+        // 애니메이션 효과
+        chart.animateXY(1000, 1000);
 
-    public void setChart()
-    {
-        // 차트 자체에 관한 것
-        hardware_chart = findViewById(R.id.hardware_chart_item_detail);
-        hardware_chart.setTouchEnabled(false);
-        review_chart = findViewById(R.id.review_chart_item_detail);
-        review_chart.setTouchEnabled(false);
-
-        hardware_chart.setWebLineWidth(1f);
-        hardware_chart.setWebColor(Color.LTGRAY);
-        hardware_chart.setWebLineWidthInner(1f);
-        hardware_chart.setWebColorInner(Color.LTGRAY);
-        hardware_chart.setWebAlpha(70);
-        hardware_chart.getDescription().setEnabled(false);
-
-        review_chart.setWebLineWidth(1f);
-        review_chart.setWebColor(Color.LTGRAY);
-        review_chart.setWebLineWidthInner(1f);
-        review_chart.setWebColorInner(Color.LTGRAY);
-        review_chart.setWebAlpha(70);
-        review_chart.getDescription().setEnabled(false);
-
-        // create a custom MarkerView (extend MarkerView) and specify the layout
-        // to use for it
-
-
-        // list that saves the value
-        List<RadarEntry> hardwareAL = new ArrayList<>();
-        List<RadarEntry> reviewAL = new ArrayList<>();
-        // 여기만 수정하며 됨
-        for (int i = 0; i < 6; i++) {
-
-            float val1 = (float) (Math.random() * 6);
-            float val2 = (float) (Math.random() * 6);
-            hardwareAL.add(new RadarEntry(i, val1));
-            reviewAL.add(new RadarEntry(i, val2));
-        }
-
-        RadarDataSet hardwareSet = new RadarDataSet(hardwareAL, "");
-        RadarDataSet reviewSet = new RadarDataSet(reviewAL, "");
-
-        hardwareSet.setFillColor(hardwareColor);
-        hardwareSet.setDrawFilled(true);
-        hardwareSet.setLineWidth(2f);
-        hardwareSet.setFillAlpha(30);
-
-        reviewSet.setFillColor(reviewColor);
-        reviewSet.setDrawFilled(true);
-        reviewSet.setLineWidth(2f);
-        reviewSet.setFillAlpha(30);
-
-
-        ArrayList<IRadarDataSet> hardware_sets = new ArrayList<>();
-        hardware_sets.add(hardwareSet);
-        ArrayList<IRadarDataSet> review_sets = new ArrayList<>();
-        review_sets.add(reviewSet);
-
-
-        RadarData hardware_data = new RadarData(hardware_sets);
-        hardware_data.setDrawValues(false);
-        RadarData review_data = new RadarData(review_sets);
-        review_data.setDrawValues(false);
-
-        // set color of the chart
-        hardwareSet.setColor(hardwareColor);
-        reviewSet.setColor(reviewColor);
-        hardware_chart.setData(hardware_data);
-        hardware_chart.invalidate();
-        review_chart.setData(review_data);
-        review_chart.invalidate();
-
-        // set data
-
-        //chart.animateXY(1400, 1400, Easing.EaseInOutQuad);
-
-        /*
         XAxis xAxis = chart.getXAxis();
-        //xAxis.setTypeface(tfLight);
-        xAxis.setTextSize(9f);
+        xAxis.setTextSize(15f);
         xAxis.setYOffset(0f);
         xAxis.setXOffset(0f);
-        xAxis.setValueFormatter(new IndexAxisValueFormatter() {
+        xAxis.setValueFormatter(new MyValueFormatter());
+        xAxis.setTextColor(Color.BLACK);
 
-            private final String[] mActivities = new String[]{"Burger", "Steak", "Salad", "Pasta", "Pizza"};
-
-            public String getFormattedValue(float value) {
-                return mActivities[(int) value % mActivities.length];
-            }
-        });
-        xAxis.setTextColor(Color.WHITE);
         YAxis yAxis = chart.getYAxis();
-        //yAxis.setTypeface(tfLight);
-        yAxis.setLabelCount(5, false);
+        yAxis.setLabelCount(4, false);
         yAxis.setTextSize(9f);
         yAxis.setAxisMinimum(0f);
-        yAxis.setAxisMaximum(80f);
+        yAxis.setAxisMaximum(100f);
         yAxis.setDrawLabels(false);
 
+        // 차트 어떤건지
         Legend l = chart.getLegend();
-        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
-        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
-        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
         l.setDrawInside(false);
-        //l.setTypeface(tfLight);
-        l.setXEntrySpace(7f);
-        l.setYEntrySpace(5f);
-        l.setTextColor(Color.WHITE);
-
-         */
+        l.setYOffset(-10);
+        l.setYEntrySpace(0);
+        l.setTextSize(15);
+        l.setTextColor(Color.BLACK);
     }
 
-    public class MyValueFormatter implements IValueFormatter {
+    private void setData() {
+
+        float mul = 100;
+        float min = 0;
+        int cnt = 6;
+
+        ArrayList<RadarEntry> entries1 = new ArrayList<>();
+        ArrayList<RadarEntry> entries2 = new ArrayList<>();
+
+        // 값 설정!!
+        // NOTE: The order of the entries when being added to the entries array determines their position around the center of
+        // the chart.
+        for (int i = 0; i < cnt; i++) {
+            float val1 = (float) (Math.random() * mul) + min;
+            entries1.add(new RadarEntry(val1));
+
+            float val2 = (float) (Math.random() * mul) + min;
+            entries2.add(new RadarEntry(val2));
+        }
+
+        RadarDataSet set1 = new RadarDataSet(entries1, "하드웨어 스펙");
+        set1.setColor(hardwareColor);
+        set1.setFillColor(hardwareColor);
+        set1.setDrawFilled(true);
+        set1.setFillAlpha(70);
+        set1.setLineWidth(1f);
+        //set1.setDrawHighlightCircleEnabled(true);
+        //set1.setDrawHighlightIndicators(false);
+
+        RadarDataSet set2 = new RadarDataSet(entries2, "사용자 리뷰 분석");
+        set2.setColor(reviewColor);
+        set2.setFillColor(reviewColor);
+        set2.setDrawFilled(true);
+        set2.setFillAlpha(70);
+        set2.setLineWidth(1f);
+        //set2.setDrawHighlightCircleEnabled(true);
+        //set2.setDrawHighlightIndicators(false);
+
+        ArrayList<IRadarDataSet> sets = new ArrayList<>();
+        sets.add(set1);
+        sets.add(set2);
+
+        RadarData data = new RadarData(sets);
+        data.setDrawValues(false);
+        // 데이터 값 표시
+        /*
+        data.setValueTextSize(8f);
+        data.setValueTextColor(Color.BLACK);
+         */
+
+        chart.setData(data);
+        chart.invalidate();
+    }
+
+
+    public class MyValueFormatter implements IAxisValueFormatter {
 
         private DecimalFormat mFormat;
+        private final String[] mActivities = new String[]{"무게", "디자인", "화면 크기", "성능", "그래픽", "배터리"};
 
         public MyValueFormatter() {
-            mFormat = new DecimalFormat("###,###,##0.0"); // use one decimal
+            mFormat = new DecimalFormat("###,###,##0"); // use one decimal
         }
 
         @Override
-        public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
-            // write your logic here
-            return mFormat.format(value) + " $"; // e.g. append a dollar-sign
+        public String getFormattedValue(float value, AxisBase axis) {
+            return mActivities[(int) value % mActivities.length];
         }
     }
+
 }
